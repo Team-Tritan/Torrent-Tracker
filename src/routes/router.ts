4beb4e348529afd241344a7a71e82f1bf364a2e4
big dick bandit <dylan@tritan.gg>
2026-05-11 01:17:45 -0600

    Add index.html page and update router to serve it

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -8,13 +8,17 @@ import healthRouter from "./health";
 
 const router = Router();
 
+router.get("/", (_req: Request, res: Response) => {
+  res.sendFile("index.html", { root: "src/pages" });
+});
+
 router.use("/announce", announceRouter);
 router.use("/scrape", scrapeRouter);
 router.use("/stats", statsRouter);
 router.use("/health", healthRouter);
 
 router.use((_req: Request, res: Response) => {
-  res.end();
+  res.status(404).json({ status: 404, message: "Route not found" });
 });
 
 export default router;
