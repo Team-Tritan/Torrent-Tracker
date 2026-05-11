7328571b61fa0089dccce81365f18e051f8a0019
big dick bandit <dylan@tritan.gg>
2026-05-11 00:08:51 -0600

    Refactor server.ts to load blacklist from file and remove state saving logic

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,36 @@
 "use strict";
 
 import express from "express";
+import { cleanupInactivePeers, loadBlacklist } from "./lib/store";
+import { peerExpirationTime } from "./types";
+import fs from "fs";
+import path from "path";
 import router from "./routes/router";
-import { cleanupInactivePeers, torrents } from "./lib/store";
-import { loadState, saveState, setupPeriodicStateSaving } from "./utils";
-import { defaultStateSaveInterval, peerExpirationTime } from "./types";
+import redis from "./lib/redis";
 
 const config = {
   port: parseInt(process.env.PORT || "8080"),
   cleanupInterval: process.env.CLEANUP_INTERVAL
     ? parseInt(process.env.CLEANUP_INTERVAL, 10) * 60 * 1000
     : Math.floor(peerExpirationTime / 3),
-  stateSaveInterval: defaultStateSaveInterval,
 };
 
-Object.assign(torrents, loadState());
+const blacklistPath = path.join(__dirname, "..", "data", "blacklist.json");
+try {
+  if (fs.existsSync(blacklistPath)) {
+    const blacklistData = fs.readFileSync(blacklistPath, "utf-8");
+    const blacklist = JSON.parse(blacklistData);
+    if (Array.isArray(blacklist)) {
+      loadBlacklist(blacklist);
+      console.log(`Loaded ${blacklist.length} hashes from the blacklist.`);
+    } else {
+      console.error("Blacklist is not a valid array.");
+    }
+  }
+} catch (error) {
+  const err = error as Error;
+  console.error("Error loading blacklist:", err.message);
+}
 
 const server = express()
   .disable("x-powered-by")
@@ -30,21 +46,14 @@ const server = express()
 
 const peerCleanupInterval = setInterval(
   cleanupInactivePeers,
-  config.cleanupInterval
+  config.cleanupInterval,
 );
 
-const stateSavingInterval = setupPeriodicStateSaving(
-  torrents,
-  config.stateSaveInterval
-);
-
-const handleShutdown = (): void => {
+const handleShutdown = async (): Promise<void> => {
   console.log("Shutting down tracker...");
 
   clearInterval(peerCleanupInterval);
-  clearInterval(stateSavingInterval);
-
-  saveState(torrents);
+  await redis.quit();
   process.exit(0);
 };
 
